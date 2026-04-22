bf15305e239e4585e8750cf704c00a7e239cd41c
grhandge <handgeg7@example.com>
2026-04-22 13:45:59 +0530

    jenkinsfile  add 5

--- AKS-Terraform-GitHub-Actions/aks/backend.tf
+++ AKS-Terraform-GitHub-Actions/aks/backend.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfffstateaccount"
     container_name       = "tfstate"
     key                  = "aks/terraform.tfstate"
   }
